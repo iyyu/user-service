09982c4151922a135b4ae69c75dc2314d4d4246e
Iris Yu <iyucomms@gmail.com>
2018-02-10 15:24:01 -0800

    Add insertToUsers function, clean up code

--- database/index.js
+++ database/index.js
@@ -1,27 +1,34 @@
+require("dotenv").load();
 const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'psql://MacbookPro:@localhost/capstone';
+
+const connectionString = process.env.DATABASE_URL;
 
 const client = new pg.Client(connectionString);
 
-client.connect()
+client
+.connect()
 .then(() => console.log('Connected to the database!'))
 .catch(err => console.error('Error connecting to the database', err.stack));
 
 
 /* Insertion queries */
-const insertToUsers = values => {
-  const query = {
-    text: 'INSERT INTO users (username, email, country, birthdate, lastlogin, isartist, ispremium, image) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
-    values: [values]
-  }
+const insertToUsers = obj => {
+  let image = obj.image || 'https://www.poynter.org/sites/default/files/wp-content/uploads/2012/09/default_profile_2_reasonably_small.png';
+  const query = { 
+    text: "INSERT INTO users (username, email, country, birthdate, lastlogin, isartist, ispremium, image) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *", 
+    values: [obj.username, obj.email, obj.country, obj.birthdate, obj.lastlogin, obj.isartist, obj.ispremium, image]
+  };
   return new Promise ((resolve, reject) => {
-    console.time('insertion');
+    console.time(`insertion of ${obj.username}`);
     client.query(query)
-    .then(results => {
-      console.timeEnd('insertion');
-      resolve(results);
+    .then(newUserProfile => {
+      console.timeEnd(`insertion of ${obj.username}`);
+      resolve(newUserProfile);
     })
-    .catch(err => console.error(err.stack));
+    .catch(err => {
+      console.error(err.stack);
+      reject(err);
+    });
   });
 }
 
@@ -35,10 +42,17 @@ const selectUserByUserId = userId => {
   return new Promise((resolve, reject) => {
     client.query(query)
     .then(results => {
-      console.timeEnd(`search for ${userId}`);
-      resolve(results.rows[0]);
+      if (results.rows.length) {
+        console.timeEnd(`search for ${userId}`);
+        resolve(results.rows[0]);
+      } else {
+        reject(results);
+      }
     })
-    .catch(err => console.error(err.stack));
+    .catch(err => {
+      console.error(err.stack);
+      reject(err);
+    });
   });
 }
 
